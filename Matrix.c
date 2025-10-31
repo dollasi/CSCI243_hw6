f2479904043321da9785ac7d74770e3d7e232a89
Alexandria Lynne Jones <alj5312@rit.edu>
2025-10-31 10:39:20 -0400

    added structs, matrix methods, and statuses (bare bones)

---
+++ Matrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Matrix.h"
+
+struct matrix_st{}
+
+Matrix mat_create(){}
+
+void mat_destroy(){}
+
+void mat_init(){}
+
+Matrix mat_duplicate(){}
+
+bool mat_equals(){}
+
+void mat_scalar_mult(){}
+
+Matrix mat_mult(){}
+
+Status mat_get_cell(){}
+
+Status mat_get_row(){}
+
+Status mat_set_cell(){}
+
+Status mat_set_row(){}
+
+Matrix mat_transpose(){}
+
+void mat_print(){}
